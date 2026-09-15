c94e7fc968ab387c22a116625fd795c88cbf60a2
Manuella <manuferreira957@gmail.com>
2026-09-15 09:30:40 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("");
   console.log("=================================");
-  console.log("🏥 HospTech iniciado pelo index.js!");
+  console.log("🏥 HospTech iniciado!");
   console.log(`🌐 http://localhost:${PORT}`);
   console.log("=================================");
 });
